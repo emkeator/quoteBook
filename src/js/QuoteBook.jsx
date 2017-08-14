eb54f39302b91d24367c904b622beb9297b13faa
brennongs <brennonschow@gmail.com>
2017-08-14 16:20:27 -0600

    Updates

--- src/js/QuoteBook.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class QuoteBook extends Component {
-    render() {
-        return (
-            <h1>QuoteBook</h1>
-        )
-    }
-}
--- src/js/Quotes.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class Quotes extends Component {
-    render() {
-        return (
-            <h1>new quotes</h1>
-        )
-    }
-}
