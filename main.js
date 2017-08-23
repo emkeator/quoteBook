8df7cb256b7d980af17d28337c48afecdf31a549
brennongs <brennonschow@gmail.com>
2017-08-23 09:10:53 -0600

    changed back end arcitechture

--- main.js
+++ main.js
@@ -2,8 +2,7 @@ const express=require('express'),
 	  session=require('express-session'),
 	  bodyParser=require('body-parser'),
 	  cors=require('cors'),
-	  path=require('path'),
-	  quotesCtrl=require('./server/quotesCtrl'),
+	  quotes=require('./server/quotes'),
 	  auth=require('./server/auth'),
 	  { port }=require('./server/config.json'),
 	  app=express()
@@ -13,13 +12,6 @@ app.use(cors())
 app.use(express.static('./build'))
 
 auth(app)
+quotes(app)
 
-app.get('/api/quote', quotesCtrl.getQuote)
-//PUT
-
-//POST
-
-//DELETE
-
-// app.get('/*',(req, res)=>{res.sendFile(path.join(__dirname, 'build', 'index.html'))})
 app.listen(port, _=>{console.log('(I)...(I) '+port)})
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,21 @@
 import axios from 'axios'
 
-const URL = '/api/'
+const URL = 'http://localhost:3001/api/'
 
 export async function getNewQuote(){
     try {
         return await axios.get(`${URL}quote`)
     }
     catch(err){
-        console.log(err)
+        throw err
+    }
+}
+
+export async function login(){
+    try {
+        axios.get(`${URL}auth/login`)
+    }
+    catch(err){
+        throw err
     }
 }
